bcfe2fda5c29386abd38a57b7656b28992bc2d05
Masayuki Ota <masota@microsoft.com>
2020-04-21 11:16:07 +0900

    add enzyme test

--- person-list/src/App.test.tsx
+++ person-list/src/App.test.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
+import UserList from './components/UserList/UserList';
+import Auth from './components/Auth/Auth';
 
 test('renders span', () => {
-  const { getByText, container, getByTestId } = render(<App />);
-  const spanElement = getByText('Person List');
-  const span2Element = container.querySelector('span')?.innerHTML;
-  expect(spanElement).toBeInTheDocument();
-  expect(getByTestId('person-list-stack')).toBeInTheDocument();
-  expect(span2Element).toBe('Person List');
+  // Arrange
+  const appWrapper = shallow(<App />);
+
+  // Act & Assert
+  expect(appWrapper.find(Auth).length).toBe(1);
+  expect(appWrapper.find(UserList).length).toBe(1);
 });
